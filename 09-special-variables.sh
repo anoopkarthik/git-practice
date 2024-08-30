78053d6d268227565c646aafbd677571c61c02c6
Karthik <anoopkarthikaggala@gmail.com>
2024-08-30 19:07:45 +0530

    variables

--- 09-special-variables.sh
+++ 09-special-variables.sh
@@ -4,3 +4,4 @@ echo "All variables are passed to the script: $@"
 echo "Number of variables passed: $#"  #echo
 echo "Script name: $0"
 echo "current working directory: $PWD"
+echo "Home directory of current user: $HOME"
