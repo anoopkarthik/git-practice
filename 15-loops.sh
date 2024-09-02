26d7c39e7d49ff74e06dade97235640e2ce2bc5b
Karthik <anoopkarthikaggala@gmail.com>
2024-09-02 17:02:08 +0530

    shell

--- 15-loops.sh
+++ 15-loops.sh
@@ -34,5 +34,16 @@ CHECKROOT
 #sh 15-loops.sh git mysql postfix nginx
 for package in $@ #$@ refers to all arguments passed to it
 do 
-    echo $package 
+    dnf list installed $package
+    if [ $? -ne 0 ]
+    then
+         echo "$package is not installed , going to install it..."
+        dnf install $package -y
+
+        VALIDATE $? "Installing $package"
+
+    else
+    echo "$package is alreday installed, nothing to do"
+    fi
+
 done
