c93e4f08c8ab5bb1653c0f1b39dc30f624e981e8
Karthik <anoopkarthikaggala@gmail.com>
2024-09-02 18:19:53 +0530

    shell

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -7,7 +7,6 @@ LOG_FILE=$LOG_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER
 
 
-
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
@@ -21,7 +20,6 @@ CHECK_ROOT(){
         echo -e "$R Please run this script with root priveleges $N" &>>$LOG_FILE
         exit 1
     fi
-
 }
 
 VALIDATE(){
@@ -31,7 +29,7 @@ VALIDATE(){
         exit 1
     else 
         echo -e "$2 is .. $G SUCCESS $N" &>>$LOG_FILE
-        fi
+    fi
 }
 
 USAGE(){
